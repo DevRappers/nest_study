d9168e7685e913a3a6e08db4ee186aa0baa6edd8
DevRappers <devrappers@gmail.com>
2020-11-17 10:16:40 +0900

    [ADD] decorator study

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,30 +1,37 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
-
   @Get()
-  getAll(){
-    return "This will return all movies"
+  getAll() {
+    return 'This will return all movies';
   }
 
-  @Get("/:id")
-  getOne(@Param("id") movieId:string){
-    return `This will return one movie with the id: ${movieId}`
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie mad after: ${searchingYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create(){
-    return "This will create a movie";
+  create(@Body() movieData) {
+    return movieData;
   }
 
-  @Delete("/:id")
-  remove(@Param("id") movieId: string){
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param("id") movieId: string){
-    return `This will patch a movie with the id ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
